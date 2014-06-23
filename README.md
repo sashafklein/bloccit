55495c812bf6f3c57b112eda7bd80dfdc6240b40
Sasha Klein <sashafklein@gmail.com>
2014-06-23 14:30:06 -0700

    Basic Rails App

---
+++ README.md
@@ -0,0 +1,4 @@
+## Bloccit Sample App
+
+Each commit tracks a commit in the Bloccit tutorial on [Bloc](https://www.bloc.io).
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = '1a08ea68cd5861e258055e56880d3e6f6f8fbd2d52dc22ec66b410167743773a438f37447d1cfa70022c9df264fee500a88e75e90adc3b971fe7b85141e4c7a5'
